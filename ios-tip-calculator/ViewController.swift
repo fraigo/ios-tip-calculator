36979cb5ac4b5d4b15c1901dec2b813adb78ff02
Francisco Igor <franciscoigor@gmail.com>
2018-10-22 21:16:30 -0700

    Added calculator and text fields

--- ios-tip-calculator/ViewController.swift
+++ ios-tip-calculator/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipPercentageTextField: UITextField!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyBoardNotification(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+    }
+    
+    @objc func keyBoardNotification(notification:NSNotification){
+        print("Keyboard on")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func touchUpInside(_ sender: Any) {
+        let amount = Int(billAmountTextField.text!) ?? 0
+        let percentage = Int(tipPercentageTextField.text!) ?? 0
+        tipAmountLabel.text = String(amount * percentage / 100)
+    }
+    
 
 }
 
